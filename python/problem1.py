80d5e071215f02978faa804d3a8db0901a3e84f7
Dan Douthit <ddouthit@rubiconproject.com>
2025-12-18 21:33:06 -0700

    Solved Problem 1 Part 1

--- python/problem1.py
+++ python/problem1.py
@@ -1,20 +1,19 @@
 position = 50 # initial position on the dial is 50
 password_counter_inc = 0
-iteration = 0 
-with open("/home/dandouthit/advent-of-code-2025/input/day1.txt", 'r') as file:
+with open("input/day1.txt", 'r') as file:
     for line in file:
         direction = line[0]
 
-        if direction == "L":
+        if str(direction) == "L":
             movement = (int(line[1:]) % 100) * -1
-        elif direction == "R":
+        elif str(direction) == "R":
             movement = (int(line[1:]) % 100)
 
         new_position = position + movement
         if new_position < 0:
             position = 100 + new_position
-        elif new_position > 100:
-            position -= 100
+        elif new_position > 99:
+            position = new_position - 100
         else:
             position = new_position
 
